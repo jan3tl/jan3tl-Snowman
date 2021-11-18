ca39b84d96f2eb779a95ae0c4adfc7c210eb3b39
jan3tl <j1lim2023@chadwickschool.org>
2021-11-18 11:40:58 -0800

    m

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public GameObject StartScreen;
+public GameObject PlayScreen;
+
 public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
@@ -11,6 +14,14 @@ public class GameController : MonoBehaviour
     {
         this.Message.text = "Can you save the Snowman?";
         this.StartButton.gameObject.SetActive(false);
+        this.StartScreen.GameObject.SetActive(false);
+        this.PlayScreen.GameObject.SetActive(true);
+    }
+
+    public void OpenStartScreen()
+    {
+        this.StartScreen.GameObject.SetActive(true);
+        this.PlayScreen.GameObject.SetActive(false);
     }
 
 }
