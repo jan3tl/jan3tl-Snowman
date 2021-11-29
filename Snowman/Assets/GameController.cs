3ab36e3073b1be414e52cd20dc1d923b98c26553
jan3tl <j1lim2023@chadwickschool.org>
2021-11-29 12:20:50 -0800

    library adding

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WordGuesser;
 
 
 public class GameController : MonoBehaviour
@@ -8,21 +9,23 @@ public class GameController : MonoBehaviour
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Button StartButton;
     public GameObject StartScreen;
-    public GameObject PlayScreen; 
-
+    public GameObject PlayScreen;
+    private WordGuesser.WordGame guessingGame;
     public void StartGame()
     {
+        this.guessingGame = new WordGuesser.WordGame("apple", 5);
+        Debug.Log(this.guessingGame.GetWord());
+        Debug.Log(this.guessingGame.GetFullWord());
+
         this.StartScreen.SetActive(false);
         this.PlayScreen.SetActive(true);
-       
-        
     }
 
     public void OpenStartScreen()
     {
         this.PlayScreen.SetActive(false);
         this.StartScreen.SetActive(true);
-    
+
     }
 
     public void Start()
