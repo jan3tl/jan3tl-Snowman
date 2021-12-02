901b16504e26462466ae0a7ca07c4a1b77156fcf
jan3tl <j1lim2023@chadwickschool.org>
2021-12-02 12:21:46 -0800

    milestone 5

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -7,13 +7,24 @@ using WordGuesser;
 public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
+    public UnityEngine.UI.Text GetWord;
+    public UnityEngine.UI.Text GetGuessedLetters;
+    public UnityEngine.UI.Text CheckGuess;
+    public UnityEngine.UI.Text TurnsLeft;
     public UnityEngine.UI.Button StartButton;
     public GameObject StartScreen;
     public GameObject PlayScreen;
+    public GameObject GameOver;
+    public GameObject GameWon;
     private WordGuesser.WordGame guessingGame;
+    public UnityEngine.UI.InputField PlayerGuess;
+
+    WordGuesser.WordSelector mySelector = WordGuesser.WordSelector.LoadFromString("apple banana grape");
+
     public void StartGame()
     {
-        this.guessingGame = new WordGuesser.WordGame("apple", 5);
+        string randomWord = mySelector.GetWord();
+        //this.guessingGame = new WordGuesser.WordGame("apple", 5);
         Debug.Log(this.guessingGame.GetWord());
         Debug.Log(this.guessingGame.GetFullWord());
 
@@ -23,8 +34,11 @@ public class GameController : MonoBehaviour
 
     public void OpenStartScreen()
     {
+        this.GameWon.SetActive(false);
+        this.GameOver.SetActive(false);
         this.PlayScreen.SetActive(false);
         this.StartScreen.SetActive(true);
+        
 
     }
 
@@ -33,6 +47,29 @@ public class GameController : MonoBehaviour
         this.PlayScreen.SetActive(false);
         this.StartScreen.SetActive(true);
     }
+
+    public void SubmitGuess()
+    {
+       
+       Debug.Log(this.guessingGame.CheckGuess(this.PlayerGuess.text));
+       
+       GetGuessedLetters.text = this.guessingGame.GetGuessedLetters();
+       GetWord.text = this.guessingGame.GetWord();
+       
+       if(this.guessingGame.IsGameWon())
+       {
+           this.GameWon.SetActive(true);
+       }
+       else if (this.guessingGame.IsGameOver())
+       {
+           this.GameOver.SetActive(true);
+       }
+       
+
+       PlayerGuess.text = string.Empty;
+
+    }
+
 
 }
 
